11caa7602b9a3b629bd6630c01ab80796d2ee928
Phani2425 <mohantyphanibhusan@gmail.com>
2024-09-01 15:02:45 +0530

    add confirmation modal for account deletion

--- src/Components/core/Dashboard/DeleteAccount.jsx
+++ src/Components/core/Dashboard/DeleteAccount.jsx
@@ -1,21 +1,26 @@
-import { FiTrash2 } from "react-icons/fi"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { deleteProfile } from "../../../Services/operations/profileAPI"
-
+import { FiTrash2 } from "react-icons/fi";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { deleteProfile } from "../../../Services/operations/profileAPI";
+import { useState } from "react";
+import ConfirmationModal from "../Common/ConfirmationModal";
 
 export default function DeleteAccount() {
-  const { token } = useSelector((state) => state.auth)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   async function handleDeleteAccount() {
     try {
-      dispatch(deleteProfile(token, navigate))
+      dispatch(deleteProfile(token, navigate));
     } catch (error) {
-      console.log("ERROR MESSAGE - ", error.message)
+      console.log("ERROR MESSAGE - ", error.message);
     }
   }
+
+  const [showConfirmationModal, setshowConfirmationModal] = useState(false);
+  //user is  required to check wheathe it is student or instructor and showing  message in confirmation modal according to it
+  const { user } = useSelector((state) => state.profile);
 
   return (
     <>
@@ -37,12 +42,31 @@ export default function DeleteAccount() {
           <button
             type="button"
             className="w-fit cursor-pointer italic text-pink-300"
-            onClick={handleDeleteAccount}
+            onClick={() => {
+              setshowConfirmationModal(true);
+            }}
           >
             I want to delete my account.
           </button>
         </div>
       </div>
+
+      {showConfirmationModal && (
+        <ConfirmationModal
+          text1={"Do you really want to delete your account ?"}
+          text2={
+            user.accountType === "Student"
+              ? "Your all purchased courses and course progresses will be lost and you will not be able to access them again...."
+              : "All of your created courses will be lost and you will not be able to access them again...."
+          }
+          btn1Text="Delete"
+          btn2Text="Nah, Just Kidding"
+          btn1Handler={handleDeleteAccount}
+          btn2Handler={() => {
+            setshowConfirmationModal(false);
+          }}
+        />
+      )}
     </>
-  )
+  );
 }
